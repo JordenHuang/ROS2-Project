c1959a5c6efdd4e0dca9f1b424de56b59e22d2fd
JordenHuang <jorden90573@gmail.com>
2025-09-30 17:39:25 +0800

    Update my_create, make it work with real robot

--- src/my_create/launch/mycreate_launch.py
+++ src/my_create/launch/mycreate_launch.py
@@ -1,35 +1,126 @@
-import os
-import launch
-from launch import LaunchDescription
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from webots_ros2_driver.webots_launcher import WebotsLauncher
-from webots_ros2_driver.webots_controller import WebotsController
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import os
 
 def generate_launch_description():
     package_dir = get_package_share_directory('my_create')
-    robot_description_path = os.path.join(package_dir, 'resource', 'MyCreate.urdf')
-    world_path = os.path.join(package_dir, 'worlds', 'school-obstacle.wbt')
 
-    webots = WebotsLauncher(
-        world=world_path
+    rviz_config_file = os.path.join(package_dir, 'config', 'rviz_config.rviz')
+    nav2_params_file = os.path.join(package_dir, 'config', 'nav2_params.yaml')
+    explore_params_file = os.path.join(package_dir, 'config', 'explore.yaml')
+
+    # Directories
+    pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
+
+    # Paths
+    # Our launch file has the same name as the nav2_bringup one
+    nav2_launch = PathJoinSubstitution(
+        [pkg_nav2_bringup, 'launch', 'navigation_launch.py'])
+
+    # Includes
+    nav2 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([nav2_launch]),
+        launch_arguments=[
+            ('use_sim_time', 'false'),
+            ('params_file', nav2_params_file)
+        ]
     )
 
-    my_robot_driver = WebotsController(
-        robot_name='MyCreate',
-        parameters=[
-            {'robot_description': robot_description_path},
+    explore_lite = Node(
+        package="explore_lite",
+        name="explore_node",
+        executable="explore",
+        parameters=[explore_params_file, {"use_sim_time": False}],
+        output="screen",
+        remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
+    )
+
+    depth_image_to_laserscan = Node(
+        package="depthimage_to_laserscan",
+        executable="depthimage_to_laserscan_node",
+        output='screen',
+        namespace="d2l",
+        name="depth_image_to_laserscan",
+        parameters=[{
+# <param name="scan_height"     type="int"    value="1"/> <!-- default: 1 pixel. Number of pixel rows used to generate laser scan. -->
+# <param name="scan_time"       type="double" value="0.033"/> <!-- default:0.033, 30 FPS . Time between scans. -->
+# <param name="range_min"       type="double" value="0.45"/> <!--default:0.45m. Ranges less than this are considered -Inf. -->
+# <param name="range_max"       type="double" value="10.0"/> <!--default: 10m. Ranges less than this are considered +Inf. -->
+# <param name="output_frame_id" type="str"    value="camera_depth_frame"/> <!--default: camera_depth_frame. Frame id of the laser scan. -->
+            "range_max": 4.0,
+            "range_min": 0.01,
+            "scan_height": 10,
+            "use_sim_time": False,
+        }],
+        remappings=[
+            ("depth", "/camera/camera/depth/image_rect_raw"),
+            ("depth_camera_info", "/camera/camera/depth/camera_info"),
+        ]
+    )
+
+    # RTAB-Map SLAM Node
+    rtabmap_slam_node = Node(
+        package='rtabmap_slam',
+        executable='rtabmap',
+        name='rtabmap',
+        output='screen',
+        parameters=[{
+            'use_sim_time': False,
+            'frame_id': 'base_link',
+        'subscribe_rgbd': True,
+            # 'subscribe_depth': True,
+            'subscribe_scan': True,
+            'subscribe_odom_info': False, # Set to false because we uses wheel odometry now
+            'publish_tf': True,
+            'approx_sync': True,
+        'approx_sync_max_interval': 0.1,
+
+            'map_always_update': True,
+            'sync_queue_size': 30,
+            'topic_queue_size': 30,
+
+            "RGBD/ProximityBySpace": "false",
+            "RGBD/AngularUpdate": "0.01",
+            "RGBD/LinearUpdate": "0.01",
+            "RGBD/OptimizeFromGraphEnd": "false",
+            "RGBD/CreateOccupancyGrid": "true",
+            "Reg/Force3DoF": "true",
+            "Vis/MinInliers": "12",
+
+            "Grid/Sensor": "2",
+            'Grid/MaxObstacleHeight': '0.4',
+            'Grid/NormalsSegmentation': 'false',
+            "Grid/RayTracing": "true",
+            'Grid/RangeMax': '4.0',
+            'Grid/RangeMin': '0.1',
+            "Rtabmap/StartNewMapOnLoopClosure": "true",
+        }],
+        remappings=[
+            # ('rgb/image', '/camera/camera/color/image_raw'),
+            # ('rgb/camera_info', '/camera/camera/color/camera_info'),
+            # ('depth/image', '/camera/camera/depth/image_rect_raw'),
+            ('rgbd_image', 'rgbd_image/compressed'),
+            ('scan', '/d2l/scan'),
+            ('odom', '/odometry/filtered'),
         ],
-        respawn=True
+        arguments=['-d',]# "--udebug"]
+    )
+
+    rviz2 = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=['-d', rviz_config_file, '--ros-args', '--log-level', 'warn'],
+        parameters=[{'use_sim_time': False}],
     )
 
     return LaunchDescription([
-        webots,
-        my_robot_driver,
-        launch.actions.RegisterEventHandler(
-            event_handler=launch.event_handlers.OnProcessExit(
-                target_action=webots,
-                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-            )
-        )
+        rtabmap_slam_node,
+        nav2,
+        # explore_lite,
+        rviz2,
     ])
--- src/my_create/setup.py
+++ src/my_create/setup.py
@@ -11,43 +11,23 @@ setup(
     data_files=[
         # 套件資源索引
         ('share/ament_index/resource_index/packages', [f'resource/{package_name}']),
-        
         # 套件基本描述
         (f'share/{package_name}', ['package.xml']),
-
         # 啟動檔（launch）
         (f'share/{package_name}/launch', glob('launch/*.py')),
-
-        # 世界檔（Webots world）
-        (f'share/{package_name}/worlds', glob('worlds/*')),
-
-        # URDF 模型
-        (f'share/{package_name}/resource', glob('resource/*.urdf')),
-
-        # PROTO 檔（Webots）
-        (f'share/{package_name}/protos', glob('protos/*.proto')),
-
-        # PROTO (create_icons)
-        (f'share/{package_name}/protos/create_icons', glob('protos/create_icons/*')),
-
-        # PROTO (create_textures)
-        (f'share/{package_name}/protos/create_textures', glob('protos/create_textures/*')),
+        # Config files
+        (f'share/{package_name}/config', glob('config/*')),
     ],
-    
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jordenhuang',
     maintainer_email='jorden90573@gmail.com',
-    description='ROS2 package for MyCreate robot integrated with Webots',
+    description='ROS2 package for MyCreate robot',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # 'my_robot_driver = my_create.my_robot_driver:main',
-            'vision_node = my_create.vision_node:main',
         ],
-        # 'webots_ros2_driver.robot_driver': [
-        #     'MyRobotDriver = my_create.my_robot_driver:MyRobotDriver',
-        # ],
     },
 )
